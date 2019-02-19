436716acd5d35d4b10f03a8a8c0efaf0edd1f288
weigege <weigege@youqiantu.local>
2019-02-19 17:59:35 +0800

    更新startup

--- startup.sh
+++ startup.sh
@@ -3,6 +3,6 @@ if [[ -f installSSR.sh ]]
 then
 bash installSSR.sh
 else
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR.sh?token=AJWK6jrZc5Uvu4j8Oxy84cgnuNelZxn4ks5ca9DswA%3D%3D > installSSR.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR.sh?token=AJWK6q25_qKiz55L2AAk6kvGfur-w4Snks5ca9OjwA%3D%3D > installSSR.sh
 bash startup.sh
 fi
