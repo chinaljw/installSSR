70978b6a8dfc757fb1ae5f70c31c723c95112a6e
weigege <781342306@qq.com>
2019-09-19 21:05:56 +0800

    下载端口ip查询

--- startup-supreme.sh
+++ startup-supreme.sh
@@ -3,6 +3,7 @@ if [[ -f installSSR.sh ]]
 then
 bash installSSR.sh
 else
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > connectedIPs.sh
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme.sh > installSSR-supreme.sh
 bash installSSR-supreme.sh
 fi
