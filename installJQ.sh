7b5e5b5e1c03664c040671d7325c70a5ab1ff222
weigege <781342306@qq.com>
2019-10-17 17:30:11 +0800

    update startup;add installJQ.sh

---
+++ installJQ.sh
@@ -0,0 +1,4 @@
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum repolist
+echo "y" | yum install jq
--- startup-supreme.sh
+++ startup-supreme.sh
@@ -4,6 +4,8 @@ then
 bash installSSR-supreme.sh
 else
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > connectedIPs.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installJQ.sh > installJQ.sh
+bash installJQ.sh
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme.sh > installSSR-supreme.sh
 bash installSSR-supreme.sh
 fi
