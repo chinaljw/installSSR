7404b51c6e278b5ecca6737c72a807d42575b4d0
可以用中文嘛 <781342306@qq.com>
2019-12-01 00:29:10 +0800

    Update installSSR-supreme.sh

--- installSSR-supreme.sh
+++ installSSR-supreme.sh
@@ -19,15 +19,14 @@ echo -e "reinstall kernal..."
 rpm -ivh http://xz.wn789.com/CentOSkernel/kernel-2.6.32-504.3.3.el6.x86_64.rpm --force
 touch kernalReinstalled
 reboot
+touch reboot also continue!
 fi
 
 #install ribu
-if [[ -f kernalReinstalled && !(-f rebuInstalled) ]]
+if [[ -f kernalReinstalled && !(-f 91yunserverspeeder) ]]
 then
 echo -e "install rebu..."
-wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh
-sh serverspeeder.sh > log
-touch rebuInstalled
+wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh > sh serverspeeder.sh > log
 else
 echo -e "skip install ribu"
 fi
