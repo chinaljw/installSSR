8f40c80fdb57f88b142228c287a83a2e82f0df5d
weigege <781342306@qq.com>
2019-12-05 02:13:20 +0800

    添加启动脚本

--- installSSR-supreme.sh
+++ installSSR-supreme.sh
@@ -27,7 +27,8 @@ fi
 if [[ -f kernalReinstalled && ! -f 91yunserverspeeder ]]
 then
 echo -e "install rebu..."
-wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh & sh serverspeeder.sh > log
+wget -N --no-check-certificate https://github.com/91yun/serverspeeder/raw/master/serverspeeder.sh
+sh serverspeeder.sh > serverspeeder.log
 
 echo send email...
 email=""
--- startup-supreme-vultr.sh
+++ startup-supreme-vultr.sh
@@ -1,5 +1,10 @@
 if [[ ! -f startup-supreme.sh ]];
 then
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/startup-supreme.sh > startup-supreme.sh
+chmod +x startup-supreme.sh
 fi
-sh startup-supreme.sh {gmailEmail} {gmailPassward} >>> startup.log
+gmailEmail=
+gmailPassward=
+sh startup-supreme.sh "${gmailEmail}" "${gmailPassward}" >> startup.log
+echo -e "\n" >> /etc/rc.d/rc.local
+echo "cd ~ && sh startup-supreme.sh ${gmailEmail} ${gmailPassward} >> startup.log" >> /etc/rc.d/rc.local
