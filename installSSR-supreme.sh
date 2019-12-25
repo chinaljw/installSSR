f56d98e7a678170075718325914c42a99650ad3b
可以用中文嘛 <781342306@qq.com>
2019-12-25 10:58:26 +0800

    Update installSSR-supreme.sh

--- installSSR-supreme.sh
+++ installSSR-supreme.sh
@@ -38,7 +38,7 @@ do
 done
 echo "to ${email}"
 ip=`ifconfig | sed -n '/inet addr/s/^[^:]*:\([0-9.]\{7,15\}\) .*/\1/p' | grep -v 127.0.0.1`
-des="[Server: ${ip}] Installation has been complete."
+des="[Server: ${ip}] The installation is complete."
 echo "${des}" | mutt "${email}" -s "${des}"
 else
 echo -e "skip install ribu"
