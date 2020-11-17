1c0f6d199d47488bfd00cbe4df54a9398b3d2fdc
weigege <alan.lin@gaodun.com>
2020-11-17 14:43:27 +0800

    update

--- startup-supreme-centos7.sh
+++ startup-supreme-centos7.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 if [[ -f installSSR-supreme-centos7.sh ]]
 then
-bash installSSR-supreme.sh
+bash installSSR-supreme-centos7.sh
 else
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > connectedIPs.sh
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installJQ.sh > installJQ.sh
@@ -10,6 +10,6 @@ curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installMuttIfN
 sh installMuttIfNeeded.sh $1 $2 $3
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installHTTPD.sh > installHTTPD.sh
 sh installHTTPD.sh
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme.sh > installSSR-supreme.sh
-bash installSSR-supreme.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme-centos7.sh > installSSR-supreme-centos7.sh
+bash installSSR-supreme-centos7.sh
 fi
