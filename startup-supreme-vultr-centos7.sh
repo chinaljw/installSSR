9fe573b4e930d97b7095811c977ffbcc0d1c5eb4
weigege <alan.lin@gaodun.com>
2020-11-18 10:59:22 +0800

    update

--- startup-supreme-vultr-centos7.sh
+++ startup-supreme-vultr-centos7.sh
@@ -1,11 +1,14 @@
+gmailEmail=
+gmailPassward=
+
+cd ~
+
 if [[ ! -f startup-supreme-centos7.sh ]];
 then
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/startup-supreme-centos7.sh > startup-supreme-centos7.sh
 chmod +x startup-supreme-centos7.sh
 fi
-gmailEmail=
-gmailPassward=
-sh startup-supreme-centos7.sh "${gmailEmail}" "${gmailPassward}" >> startup.log
 chmod 777 /etc/rc.d/rc.local
 echo -e "\n" >> /etc/rc.d/rc.local
 echo "cd ~ && sh startup-supreme-centos7.sh ${gmailEmail} ${gmailPassward} >> startup.log" >> /etc/rc.d/rc.local
+sh startup-supreme-centos7.sh "${gmailEmail}" "${gmailPassward}" >> startup.log
