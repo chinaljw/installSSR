f5c5b9cec4cce8108417198d71fd7fb338e913c3
weigege <alan.lin@gaodun.com>
2020-11-18 19:03:04 +0800

    update

--- startup-supreme-centos7.sh
+++ startup-supreme-centos7.sh
@@ -6,8 +6,8 @@ else
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > connectedIPs.sh
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installJQ.sh > installJQ.sh
 bash installJQ.sh
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installMailXIfNeeded.sh > installMuttIfNeeded.sh
-sh installMuttIfNeeded.sh $1 $2 $3
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installMailXIfNeeded.sh > installMailXIfNeeded.sh
+sh installMailXIfNeeded.sh $1 $2 $3
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installHTTPD.sh > installHTTPD.sh
 sh installHTTPD.sh
 curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme-centos7.sh > installSSR-supreme-centos7.sh
