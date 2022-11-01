81e8d1be95634fb63bc8488b85305cca46f3e2ea
weigege <weigegenb666@gmail.com>
2022-11-02 00:48:40 +0800

    update startup script

--- startup-supreme-centos7.sh
+++ startup-supreme-centos7.sh
@@ -1,16 +1,17 @@
 #!bin/bash
+cd /root
 if [[ -f installSSR-supreme-centos7.sh ]]
 then
-bash installSSR-supreme-centos7.sh >> /root/installSSR-supreme-centos7.log
+bash installSSR-supreme-centos7.sh >> installSSR-supreme-centos7.log
 else
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > /root/connectedIPs.sh
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installJQ.sh > /root/installJQ.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/connectedIPs.sh > connectedIPs.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installJQ.sh > installJQ.sh
 bash installJQ.sh
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installMailXIfNeeded.sh > /root/installMailXIfNeeded.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installMailXIfNeeded.sh > installMailXIfNeeded.sh
 sh installMailXIfNeeded.sh $1 $2 $3
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installHTTPD.sh > /root/installHTTPD.sh
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installHTTPD.sh > installHTTPD.sh
 sh installHTTPD.sh
-curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme-centos7.sh > /root/installSSR-supreme-centos7.sh
-echo "@reboot sh /root/startup-supreme-centos7.sh" >> /etc/crontab
-bash installSSR-supreme-centos7.sh >> /root/installSSR-supreme-centos7.log
+curl https://raw.githubusercontent.com/chinaljw/installSSR/master/installSSR-supreme-centos7.sh > installSSR-supreme-centos7.sh
+echo "sh /root/startup-supreme-centos7.sh" >> /etc/rc.d/rc.local
+bash installSSR-supreme-centos7.sh >> installSSR-supreme-centos7.log
 fi
